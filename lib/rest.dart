b3c9f52015d62fe65345787cf191fd0b2e992555
Skycoder42 <Skycoder42@users.noreply.github.com>
2020-09-01 08:16:24 +0200

    split into 2 libraries

---
+++ lib/rest.dart
@@ -0,0 +1,13 @@
+export 'src/models/delete_request.dart';
+export 'src/models/fetch_provider_request.dart';
+export 'src/models/fetch_provider_response.dart';
+export 'src/models/oob_code_request.dart';
+export 'src/models/oob_code_response.dart';
+export 'src/models/password_reset_request.dart';
+export 'src/models/password_reset_response.dart';
+export 'src/models/refresh_response.dart';
+export 'src/models/signin_request.dart';
+export 'src/models/signin_response.dart';
+export 'src/models/update_request.dart';
+export 'src/models/update_response.dart';
+export 'src/rest_api.dart';
